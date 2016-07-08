7081d9842811c3825e3d4fca42ad46a055f570b9
Vivek Kumar Brohma <vivek.brohma@myntra.com>
2016-07-09 03:47:43 +0530

    Bug fixes

--- components/kv-pairs.jsx
+++ components/kv-pairs.jsx
@@ -1,4 +1,5 @@
-var React = require('react');
+var _ = require('underscore')
+	,React = require('react');
 
 var InputText = require('./input-text.jsx');
 
@@ -8,6 +9,18 @@ var KVPair = React.createClass({
 			pairs: []
 		};
 	},
+	hasChanged: function (pairs) {
+		var kv = {};
+
+		if(!this.props.onChange)
+			return;
+
+		_.each(pairs, function (pair) {
+			kv[pair.key] = pair.value;
+		});
+
+		this.props.onChange(this.props.name, kv);
+	},
 	updateKey: function(index, value){
 		var pairs = this.state.pairs;
 
@@ -16,6 +29,8 @@ var KVPair = React.createClass({
 		this.setState({
 			pairs: pairs
 		});
+
+		this.hasChanged(pairs);
 	},
 	updateValue: function(index, value){
 		var pairs = this.state.pairs;
@@ -25,10 +40,17 @@ var KVPair = React.createClass({
 		this.setState({
 			pairs: pairs
 		});
+
+		this.hasChanged(pairs);
 	},
-	pushPairs: function(e){
+	pushKey: function(id){
 		this.setState({
-			pairs: this.state.pairs.concat({key: e.target.value, value: ''})
+			pairs: this.state.pairs.concat({key: '', value: '', id: id})
+		});
+	},
+	pushValue: function(id){
+		this.setState({
+			pairs: this.state.pairs.concat({key: '', value: '', id: id})
 		});
 	},
 	deletePair: function(e){
@@ -43,11 +65,12 @@ var KVPair = React.createClass({
 	},
 	render: function(){
 		var self = this
-			,rows;
+			,rows
+			,now;
 
 		rows = this.state.pairs.map(function(pair, index){
 			return (
-				<div className="row has-value">
+				<div className="row has-value" key={pair.id}>
 					<div className="key-wrapper">
 						<InputText value={pair.key} onChange={self.updateKey} name={index} />
 					</div>
@@ -59,13 +82,15 @@ var KVPair = React.createClass({
 			);
 		});
 
+		now = Date.now();
+
 		rows.push(
-			<div className="row">
+			<div className="row" key={now}>
 				<div className="key-wrapper">
-					<input type="text" onFocus={this.pushPairs} value="" />
+					<InputText onFocus={this.pushKey} focusData={now} value="" />
 				</div>
 				<div className="value-wrapper">
-					<input type="text" value="" />
+					<InputText onFocus={this.pushValue} focusData={now} value="" />
 				</div>
 			</div>
 		);
