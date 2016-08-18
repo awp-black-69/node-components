d58593d0be1fa1b1703c0cac05c4dedec0cb6eee
Vivek Kumar Brohma <vivek.brohma@myntra.com>
2016-08-19 00:08:26 +0530

    Handling pre-checked option

--- components/dummy-checkbox.jsx
+++ components/dummy-checkbox.jsx
@@ -9,13 +9,36 @@ function getRndId() {
 var DummyCheckbox = React.createClass({
 	getInitialState: function () {
 		return {
-			dumId: getRndId()
+			dumId: null,
+			isChecked: null
 		};
 	},
+
+	checkboxChanged: function (e) {
+		this.setState({
+			isChecked: e.target.checked
+		});
+
+		this.props.onChange && this.props.onChange();
+	},
+	initialChecked: function (props) {
+		this.setState({
+			isChecked: props.checked,
+			dumId: props.id || getRndId()
+		});
+	},
+
+	componentDidMount: function () {
+		this.initialChecked(this.props);
+	},
+	componentWillReceiveProps: function (nextProps) {
+		this.initialChecked(nextProps);
+	},
+
 	render: function () {
 		return (
 			<div className="dummy-checkbox">
-				<input type="checkbox" id={this.state.dumId} onChange={this.props.onChange} />
+				<input type="checkbox" id={this.state.dumId} onChange={this.checkboxChanged} checked={this.state.isChecked} />
 				<label htmlFor={this.state.dumId}>
 					<span className="selection"/>
 				</label>
