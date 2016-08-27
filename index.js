5dff59368c2b2e5da7089a5df92de3af03945613
Vivek Kumar Brohma <vivek.brohma@myntra.com>
2016-08-27 18:09:36 +0530

    Adding Copy to clipboard util

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ module.exports = {
 	DummySelect: require('./components/dummy-select.jsx'),
 	InputText: require('./components/input-text.jsx'),
 	TabControl: require('./components/tab-control.jsx'),
-	KvPair: require('./components/kv-pairs.jsx')
+	KvPair: require('./components/kv-pairs.jsx'),
+
+	utilUnderscoreMixin: require('./utils/underscore-mixins'),
+	utilClipboardCopy: require('./utils/clipboard-copy')
 };
---
+++ utils/clipboard-copy.js
@@ -0,0 +1,12 @@
+module.exports = function (text) {
+	var copyDiv = document.createElement('div');
+
+	copyDiv.contentEditable = true;
+	document.body.appendChild(copyDiv);
+	copyDiv.innerHTML = text;
+	copyDiv.unselectable = "off";
+	copyDiv.focus();
+	document.execCommand('SelectAll');
+	document.execCommand("Copy", false, null);
+	document.body.removeChild(copyDiv);
+};
