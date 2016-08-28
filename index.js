c4458bd76998daa510a6010edcedd74928920ba2
Vivek Kumar Brohma <vivek.brohma@myntra.com>
2016-08-29 01:04:53 +0530

    Added loader component

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = {
 	DummyCheckbox: require('./components/dummy-checkbox.jsx'),
+	OnOffSwitch: require('./components/on-off-switch.jsx'),
 	DummySelect: require('./components/dummy-select.jsx'),
-	InputText: require('./components/input-text.jsx'),
 	TabControl: require('./components/tab-control.jsx'),
+	InputText: require('./components/input-text.jsx'),
 	KvPair: require('./components/kv-pairs.jsx'),
+	Loader: require('./components/loader.jsx'),
 	Toast: require('./components/toast.jsx'),
 
 	utilUnderscoreMixin: require('./utils/underscore-mixins'),
